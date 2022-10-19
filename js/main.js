c4b8c9c0208bf04a125180626ae948224dcf0f04
Artur Motin <artur.motin98@gmail.com>
2022-10-19 18:25:24 +0300

    22

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+document.querySelector(".learnMore").addEventListener('click', scrollDown);
 
+function scrollDown()
+{
+    const pageThree = document.querySelector(".pageThree")
+    pageThree.scrollIntoView({behavior: "smooth"})
+}
